ea20f70e0cebfdf8773562dc34c0b4e182f30b24
einsfran <einsfran@cdt.com.ar>
2018-09-27 17:45:27 -0300

    mongo conection

--- index.js
+++ index.js
@@ -27,52 +27,52 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var Schema = mongoose.Schema;
 // var User = require("./user").User;
-mongoose.connect("mongodb://cdt URG:cdt.desa.3.123@ds117623.mlab.com:17623/urgmaps");
-
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose CONECTADO ' + dbURI);
-});
-
-// If the connection throws an error
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose DESCONECTADO ERROR: ' + err);
-});
-
-// When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
-})
-
-var location_schema = new Schema({
-    IMEI:Number,
-    destination: String,
-    lat:Number,
-    lng:Number,
-    state:String
-})
-
-
-var Location = mongoose.model("Location",location_schema);
-
-
-app.post("/sendMessageLocation",function(req,res){
-    var location = new Location ({
-        IMEI:req.body.IMEI,
-        destination: req.body.destination,
-        lat:req.body.lat,
-        lng:req.body.lng,
-        state:req.body.state
-    });
-
-    location.save().then(function(us){
-      res.send(doc);
-
-    },function(err){
-        if(err){
-            res.send(String(err));
-        }
-    })
-})
+// mongoose.connect("mongodb://cdt URG:cdt.desa.3.123@ds117623.mlab.com:17623/urgmaps");
+//
+// mongoose.connection.on('connected', function () {
+//   console.log('Mongoose CONECTADO ' + dbURI);
+// });
+//
+// // If the connection throws an error
+// mongoose.connection.on('error',function (err) {
+//   console.log('Mongoose DESCONECTADO ERROR: ' + err);
+// });
+//
+// // When the connection is disconnected
+// mongoose.connection.on('disconnected', function () {
+//   console.log('Mongoose default connection disconnected');
+// })
+//
+// var location_schema = new Schema({
+//     IMEI:Number,
+//     destination: String,
+//     lat:Number,
+//     lng:Number,
+//     state:String
+// })
+//
+//
+// var Location = mongoose.model("Location",location_schema);
+//
+//
+// app.post("/sendMessageLocation",function(req,res){
+//     var location = new Location ({
+//         IMEI:req.body.IMEI,
+//         destination: req.body.destination,
+//         lat:req.body.lat,
+//         lng:req.body.lng,
+//         state:req.body.state
+//     });
+//
+//     location.save().then(function(us){
+//       res.send(doc);
+//
+//     },function(err){
+//         if(err){
+//             res.send(String(err));
+//         }
+//     })
+// })
 
 app.get('/test', function(req, res) {
   res.send('hello world');
