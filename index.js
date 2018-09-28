5a96d2cc5546f6401fe7ac671ea39b1d5bab0670
einsfran <einsfran@cdt.com.ar>
2018-09-28 15:24:51 -0300

    conecction to bd  urgmaps

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-// var mongoose = require("mongoose");
-// var cors = require('express-cross');
+const cors = require('cors');
+var app = express();
+const server = require('http').createServer(app)
+var mongoose = require("mongoose");
 
 
 //Access permits
@@ -16,63 +17,63 @@ app.use(function(req, res, next) {
 
 
 app.use(cors(true));
+app.options('*', cors());
 
 app.use(express.static('public'));
 
-app.set('port', (process.env.PORT || 5000));
+server.listen(process.env.PORT || 8080);
 
 app.use(bodyParser.json());//parsea el cuerpo y lo trata como json
 app.use( bodyParser.text({type: 'application/json'}) );
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// var Schema = mongoose.Schema;
-// var User = require("./user").User;
-// mongoose.connect("mongodb://cdt URG:cdt.desa.3.123@ds117623.mlab.com:17623/urgmaps");
-//
-// mongoose.connection.on('connected', function () {
-//   console.log('Mongoose CONECTADO ' + dbURI);
-// });
-//
-// // If the connection throws an error
-// mongoose.connection.on('error',function (err) {
-//   console.log('Mongoose DESCONECTADO ERROR: ' + err);
-// });
-//
-// // When the connection is disconnected
-// mongoose.connection.on('disconnected', function () {
-//   console.log('Mongoose default connection disconnected');
-// })
-//
-// var location_schema = new Schema({
-//     IMEI:Number,
-//     destination: String,
-//     lat:Number,
-//     lng:Number,
-//     state:String
-// })
-//
-//
-// var Location = mongoose.model("Location",location_schema);
-//
-//
-// app.post("/sendMessageLocation",function(req,res){
-//     var location = new Location ({
-//         IMEI:req.body.IMEI,
-//         destination: req.body.destination,
-//         lat:req.body.lat,
-//         lng:req.body.lng,
-//         state:req.body.state
-//     });
-//
-//     location.save().then(function(us){
-//       res.send(doc);
-//
-//     },function(err){
-//         if(err){
-//             res.send(String(err));
-//         }
-//     })
-// })
+mongoose.connect("mongodb://cdtURG:cdt.desa.3.123@ds117623.mlab.com:17623/urgmaps", { useNewUrlParser: true });
+var Schema = mongoose.Schema;
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose CONECTADO ' );
+});
+
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose ERROR: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose DESCONECTADO');
+})
+
+var location_schema = new Schema({
+    idIMEI:Number,
+    destination: String,
+    lat:Number,
+    lng:Number,
+    state:String
+})
+
+
+var Location = mongoose.model("Location",location_schema);
+
+app.post("/sendMessageLocation",function(req,res){
+    var location = new Location ({
+        IMEI:req.body.IMEI,
+        destination: req.body.destination,
+        lat:req.body.lat,
+        lng:req.body.lng,
+        state:req.body.state
+    });
+
+    location.save().then(function(us){
+      res.send(doc);
+
+    },function(err){
+        if(err){
+            res.send(String(err));
+        }
+    })
+})
 
 app.get('/test', function(req, res) {
   res.send('hello world');
