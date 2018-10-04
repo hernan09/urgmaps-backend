0aef4300eba10c59696b59c956dfe80cc8bbed6b
einsfran <einsfran@cdt.com.ar>
2018-10-04 12:37:50 -0300

    index

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors(true));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
