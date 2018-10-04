00914a077fb2db39f1d2038c7a355dd1c2d1fed1
einsfran <einsfran@cdt.com.ar>
2018-10-04 11:35:04 -0300

    index

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ var cors = require('express-cross');
 
 app.use(cors(true));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 3200));
