2a057adc676d90159c2d549f307bbf05c18db612
einsfran <einsfran@cdt.com.ar>
2018-10-04 12:35:23 -0300

    index

--- index.js
+++ index.js
@@ -104,6 +104,14 @@ app.post("/saveUser",function(req,res){
       password:"123",
       idIMEI:"1234e",
       ambulance:"abc123"
+
+      name:String,
+      lastName:String,
+      cel:Number,
+      dni:String,
+      password:String,
+      idIMEI:String,
+      ambulance:String
     });
 
     user.save().then(function(us){
@@ -120,7 +128,7 @@ app.post("/saveUser",function(req,res){
 
 app.post("/login",function(req,res){
     console.log("USER",req.body);
-    User.findOne({email:req.body.userName,password: req.body.password},'_id name  lastName  cel  dni idIMEI ambulance',function(err,doc){// este metodo encuentra todos los documentos(objeto) que sea el email y pass que pasaste en array
+    User.findOne({name:req.body.userName,password: req.body.password},'_id name  lastName  cel  dni idIMEI ambulance',function(err,doc){// este metodo encuentra todos los documentos(objeto) que sea el email y pass que pasaste en array
         if(doc){
              res.send(doc)
         }else{
