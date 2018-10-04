f6a2f31e5e00608bf6d3303f6848e4a064e49244
einsfran <einsfran@cdt.com.ar>
2018-10-04 11:04:57 -0300

    login

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 var cors = require('express-cross');
 
 app.use(cors(true));
@@ -19,26 +13,18 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 3200));
 
-// app.options('*', cors());
 app.use(bodyParser.json());//parsea el cuerpo y lo trata como json
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use( bodyParser.text({type: 'application/json'}) );
-
-// app.use(express.static('public'));
 
 const server = require('http').createServer(app)
-// server.listen(process.env.PORT || 3200);
 
 app.get('/test', function (req, res) {
   res.send('POST request to the homepage');
 });
 
-
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
 const mongoose = require("mongoose");
 
@@ -48,7 +34,6 @@ var Schema = mongoose.Schema;
 mongoose.connection.on('connected', function () {
   console.log('Mongoose CONECTADO ' );
 });
-
 
 // If the connection throws an error
 mongoose.connection.on('error',function (err) {
@@ -67,7 +52,6 @@ var location_schema = new Schema({
     lng:Number,
     state:String
 })
-
 
 var Location = mongoose.model("Location",location_schema);
 
@@ -95,4 +79,22 @@ app.post("/saveLocation",function(req,res){
     })
 
     res.send("save data");
+});
+
+
+
+app.post("/login",function(req,res){
+
+    User.findOne({email:req.body.userName,password: req.body.password},'_id name  lastName  state  cel province locality',function(err,doc){// este metodo encuentra todos los documentos(objeto) que sea el email y pass que pasaste en array
+        if(doc){
+             res.send(doc)
+        }else{
+            res.send("No se encontro al Usuario");
+
+        }
+        if(err){
+            res.send(String(err));
+        }
+    })
+
 })
